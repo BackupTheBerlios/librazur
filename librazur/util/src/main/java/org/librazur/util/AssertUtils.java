407abf5c5bb85fc581a1900bb4b9fb070af02343
romale <romale>
2005-12-05 14:26:49 +0000

    initial checkin

---
+++ librazur/util/src/main/java/org/librazur/util/AssertUtils.java
@@ -0,0 +1,87 @@
+/**
+ * $Id: AssertUtils.java,v 1.1 2005/12/05 14:26:49 romale Exp $
+ *
+ * Librazur
+ * http://librazur.info
+ * Copyright (c) 2005 Librazur
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.librazur.util;
+
+
+/**
+ * Assertion utilities.
+ */
+public final class AssertUtils {
+    private AssertUtils() {
+    }
+
+
+    /**
+     * Asserts a value is not null.
+     * 
+     * @throws NullPointerException if <code>value</code> is null
+     */
+    public static void assertNotNull(String name, Object value) {
+        if (value == null) {
+            throw new NullPointerException("Argument must not be null: " + name);
+        }
+    }
+
+
+    /**
+     * Asserts a {@link String} is not blank, as defined in
+     * {@link StringUtils#isBlank(java.lang.String)}.
+     * 
+     * @throws IllegalArgumentException if
+     *             <code>StringUtils.isBlank(value) == true</code>
+     */
+    public static void assertNotBlank(String name, String value) {
+        if (StringUtils.isBlank(value)) {
+            throw new IllegalArgumentException(
+                    "Argument must not be null or empty: " + name);
+        }
+    }
+
+
+    /**
+     * Asserts a value is equal to zero or positive.
+     * 
+     * @throws IllegalArgumentException if <code>a < 0</code>
+     */
+    public static void assertZeroOrPositive(String name, int a) {
+        if (a < 0) {
+            throw new IllegalArgumentException(
+                    "Number must be equal to zero or positive: " + name + " = "
+                            + a);
+        }
+    }
+
+
+    /**
+     * Asserts a value is equal to zero or positive.
+     * 
+     * @throws IllegalArgumentException if <code>a < 0</code>
+     */
+    public static void assertZeroOrPositive(String name, double a) {
+        if (a < 0) {
+            throw new IllegalArgumentException(
+                    "Argument must be equal to zero or positive: " + name
+                            + " = " + a);
+        }
+    }
+}
---
+++ librazur/util/src/test/java/org/librazur/util/AssertUtilsTest.java
@@ -0,0 +1,77 @@
+/**
+ * $Id: AssertUtilsTest.java,v 1.1 2005/12/05 14:26:49 romale Exp $
+ *
+ * Librazur
+ * http://librazur.info
+ * Copyright (c) 2005 Librazur
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.librazur.util;
+
+
+import junit.framework.TestCase;
+
+
+public class AssertUtilsTest extends TestCase {
+    public void testAssertNotNull() {
+        AssertUtils.assertNotNull("msg", "hello");
+        try {
+            AssertUtils.assertNotNull("msg", null);
+            fail("NullPointerException was expected");
+        } catch (NullPointerException e) {
+        }
+    }
+
+
+    public void testAssertNotBlank() {
+        AssertUtils.assertNotBlank("msg", "hello");
+        AssertUtils.assertNotBlank("msg", "  hello  ");
+        try {
+            AssertUtils.assertNotBlank("msg", null);
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            AssertUtils.assertNotBlank("msg", "");
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            AssertUtils.assertNotBlank("msg", "     ");
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+
+    public void testAssertZeroOrPositive() {
+        AssertUtils.assertZeroOrPositive("a", 0);
+        AssertUtils.assertZeroOrPositive("a", 0d);
+        AssertUtils.assertZeroOrPositive("a", 1);
+        AssertUtils.assertZeroOrPositive("a", 1d);
+        try {
+            AssertUtils.assertZeroOrPositive("a", -1);
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException e) {
+        }
+        try {
+            AssertUtils.assertZeroOrPositive("a", -1d);
+            fail("IllegalArgumentException was expected");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+}
